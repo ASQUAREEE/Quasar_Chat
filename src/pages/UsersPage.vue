44883d54456d72573489f094b0f237ca60821eff
Ashher Ali <99893402+ASQUAREEE@users.noreply.github.com>
2023-07-29 00:06:02 +0530

    Add files via upload

--- src/pages/UsersPage.vue
+++ src/pages/UsersPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="flex q-pa-md">
+  <q-page v-if="!tracker" class="flex q-pa-md">
     <q-list class="full-width" separator>
       <div class="row q-mb-lg">
         <search />
@@ -80,6 +80,14 @@
         <q-btn class="makeNew" @click="makeConnection">Make New Friends</q-btn>
       </q-item-section>
     </q-list>
+  </q-page>
+
+  <q-page v-if="tracker" class="body-loading">
+    <div class="loader">
+      <div class="inner one"></div>
+      <div class="inner two"></div>
+      <div class="inner three"></div>
+    </div>
   </q-page>
 </template>
 
@@ -226,5 +234,83 @@ export default {
 .makeNew {
   background: rgb(137, 67, 195);
   margin-top: 50px;
+}
+
+html {
+  height: 100%;
+}
+
+.body-loading {
+  background-image: radial-gradient(
+    circle farthest-corner at center,
+    #3c4b57 0%,
+    #1c262b 100%
+  );
+}
+
+.loader {
+  position: absolute;
+  top: calc(50% - 32px);
+  left: calc(50% - 32px);
+  width: 64px;
+  height: 64px;
+  border-radius: 50%;
+  perspective: 800px;
+}
+
+.inner {
+  position: absolute;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+}
+
+.inner.one {
+  left: 0%;
+  top: 0%;
+  animation: rotate-one 1s linear infinite;
+  border-bottom: 3px solid #efeffa;
+}
+
+.inner.two {
+  right: 0%;
+  top: 0%;
+  animation: rotate-two 1s linear infinite;
+  border-right: 3px solid #efeffa;
+}
+
+.inner.three {
+  right: 0%;
+  bottom: 0%;
+  animation: rotate-three 1s linear infinite;
+  border-top: 3px solid #efeffa;
+}
+
+@keyframes rotate-one {
+  0% {
+    transform: rotateX(35deg) rotateY(-45deg) rotateZ(0deg);
+  }
+  100% {
+    transform: rotateX(35deg) rotateY(-45deg) rotateZ(360deg);
+  }
+}
+
+@keyframes rotate-two {
+  0% {
+    transform: rotateX(50deg) rotateY(10deg) rotateZ(0deg);
+  }
+  100% {
+    transform: rotateX(50deg) rotateY(10deg) rotateZ(360deg);
+  }
+}
+
+@keyframes rotate-three {
+  0% {
+    transform: rotateX(35deg) rotateY(55deg) rotateZ(0deg);
+  }
+  100% {
+    transform: rotateX(35deg) rotateY(55deg) rotateZ(360deg);
+  }
 }
 </style>
